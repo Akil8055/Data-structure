69216ce40824fdd15efb32e291722b5ad1b8e77b
Akil8055 <157686433+Akil8055@users.noreply.github.com>
2024-01-25 15:00:37 +0530

    Add files via upload

---
+++ swapinng two variables using pointers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter any two values = ");
+scanf("%d%d",&a,&b);
+int *p=&a,*x=&b;
+printf("values before swapping a=%d , b=%d\n",a,b);
+*p=*p+*x;
+*x=*p-*x;
+*p=*p-*x;
+printf("values before swapping a=%d , b=%d",a,b);
+return 0;
+}
