5d963ad04d1401193ed764a6bbcb5b500f4151a1
Akil8055 <157686433+Akil8055@users.noreply.github.com>
2024-01-28 11:45:05 +0530

    Add files via upload

---
+++ reverseing a string using pointers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[25],*s,a;
+	int i,j,l;
+	printf("enter a string =");
+	gets(string);
+	printf("string before =");
+	puts(string);
+	s=string;
+	l=strlen(string);
+	for(i=0,j=l-1;i<l/2;i++,j--)
+	{
+		a=*(s+i);
+		*(s+i)=*(s+j);
+		*(s+j)=a;
+	}
+	printf("string after =%s",string);
+	return 0;
+}
