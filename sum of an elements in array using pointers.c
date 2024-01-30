acfb0c0e86cd771851284425d35655df7f946b7a
Akil8055 <157686433+Akil8055@users.noreply.github.com>
2024-01-30 13:51:18 +0530

    Update sum of an elements in array using pointers.c

--- sum of an elements in array using pointers.c
+++ sum of an elements in array using pointers.c
@@ -5,7 +5,7 @@ int main()
 	printf("enter number of elements you want to enter in array = ");
 	scanf("%d",&n);
 	int a[n];
-	printf("enter array elements");
+	printf("enter array elements\n");
 	for(i=0;i<n;i++)
     scanf("%d",&a[i]);
     int *p=&a;
